f8d427765329b90e767c646224c9bf5cf98d5332
LorGuin <114704190+LorGuin@users.noreply.github.com>
2023-11-22 18:17:08 -0300

    trabajo finalizado

--- index.js
+++ index.js
@@ -26,5 +26,21 @@ const cosasQueAprendimos = [
 ];
 
 function main() {}
+const liElements = document.querySelectorAll("li");
+const listaEl = document.querySelector(".lista");
+
+for (let i = 0; i < liElements.length; i++) {
+  liElements[i].remove();
+}
+
+for (let item of cosasQueAprendimos) {
+  const newLiEl = document.createElement("li");
+  newLiEl.textContent = item.tema;
+  const laClaseQueLeVoyAAsignarAlLi = item.class || "item";
+  newLiEl.classList.add(laClaseQueLeVoyAAsignarAlLi);
+
+  listaEl.appendChild(newLiEl);
+}
 
 main();
+
